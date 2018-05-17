b22bfe7066707d71ba59a150c926f74733a99396
Ricardo Casares <ricardo_casares@epam.com>
2018-05-17 19:26:59 +0200

    pipe sitemap

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const next = require("next");
 const polka = require("polka");
+const http = require("https");
 const { join } = require("path");
 const static = require("serve-static");
 
-const { NODE_ENV, PORT = 3000 } = process.env;
+const { BEMBA_SITEMAP_SERVICE, NODE_ENV } = process.env;
 const dir = join(__dirname, "static");
 const dev = NODE_ENV !== "production";
 const app = next({ dev });
@@ -19,6 +20,9 @@ app.prepare().then(() =>
     .get("/library", render("/library"))
     .get("/settings", render("/settings"))
     .get("/:filter/:name", render("/stations"))
+    .get("/sitemap.xml", (req, res) => {
+      https.get(BEMBA_SITEMAP_SERVICE, get => get.pipe(res)).end();
+    })
     .get("*", (req, res) => handle(req, res))
     .listen(PORT)
     .then(() => console.log(`> Server ready`))
